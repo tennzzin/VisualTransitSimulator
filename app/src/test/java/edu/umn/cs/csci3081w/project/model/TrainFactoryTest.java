c4b6982309f7e495eeb77c1b0fbc18f21b6ad951
Tenzin Chonyi <chony003@umn.edu>
2025-03-04 18:01:43 -0600

    added tests for TrainFactory

--- app/src/test/java/edu/umn/cs/csci3081w/project/model/TrainFactoryTest.java
+++ app/src/test/java/edu/umn/cs/csci3081w/project/model/TrainFactoryTest.java
@@ -2,6 +2,8 @@ package edu.umn.cs.csci3081w.project.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -75,6 +77,48 @@ public class TrainFactoryTest {
     assertTrue(vehicle1 instanceof ElectricTrain);
   }
 
+  @Test
+  public void testGenerateVehicleDieselTrain() {
+    trainFactory = new TrainFactory(storageFacility, new Counter(), 2);
+    List<Stop> stopsIn = new ArrayList<Stop>();
+    Stop stop1 = new Stop(0, "test stop 1", new Position(-93.243774, 44.972392));
+    Stop stop2 = new Stop(1, "test stop 2", new Position(-93.235071, 44.973580));
+    stopsIn.add(stop1);
+    stopsIn.add(stop2);
+    List<Double> distancesIn = new ArrayList<>();
+    distancesIn.add(0.843774422231134);
+    List<Double> probabilitiesIn = new ArrayList<Double>();
+    probabilitiesIn.add(.025);
+    probabilitiesIn.add(0.3);
+    PassengerGenerator generatorIn = new RandomPassengerGenerator(stopsIn, probabilitiesIn);
+
+    Route testRouteIn = new Route(0, "testRouteIn",
+            stopsIn, distancesIn, generatorIn);
+
+    List<Stop> stopsOut = new ArrayList<Stop>();
+    stopsOut.add(stop2);
+    stopsOut.add(stop1);
+    List<Double> distancesOut = new ArrayList<>();
+    distancesOut.add(0.843774422231134);
+    List<Double> probabilitiesOut = new ArrayList<Double>();
+    probabilitiesOut.add(0.3);
+    probabilitiesOut.add(.025);
+    PassengerGenerator generatorOut = new RandomPassengerGenerator(stopsOut, probabilitiesOut);
+
+    Route testRouteOut = new Route(1, "testRouteOut",
+            stopsOut, distancesOut, generatorOut);
+
+    Line line = new Line(10000, "testLine", "TRAIN", testRouteOut, testRouteIn,
+            new Issue());
+
+    Vehicle vehicle1 = trainFactory.generateVehicle(line);
+    assertTrue(vehicle1 instanceof ElectricTrain);
+    Vehicle vehicle2 = trainFactory.generateVehicle(line);
+    assertTrue(vehicle2 instanceof DieselTrain);
+    Vehicle vehicle3 = trainFactory.generateVehicle(line);
+    assertTrue(vehicle3 instanceof ElectricTrain);
+  }
+
   /**
    * Testing if vehicle got returned.
    */
@@ -116,9 +160,9 @@ public class TrainFactoryTest {
     trainFactory.returnVehicle(testTrain);
     assertEquals(4, trainFactory.getStorageFacility().getElectricTrainsNum());
     assertEquals(3, trainFactory.getStorageFacility().getDieselTrainsNum());
-
   }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
   /**
    * Testing the constructor for TrainFactory with night-time generation strategy.
@@ -133,4 +177,49 @@ public class TrainFactoryTest {
   }
 =======
 >>>>>>> 7674128 (Completed PassengerFactory Tests)
+=======
+  /**
+   * Testing if vehicle got returned.
+   */
+  @Test
+  public void testReturnVehicleDieselTrain() {
+    List<Stop> stopsIn = new ArrayList<Stop>();
+    Stop stop1 = new Stop(0, "test stop 1", new Position(-93.243774, 44.972392));
+    Stop stop2 = new Stop(1, "test stop 2", new Position(-93.235071, 44.973580));
+    stopsIn.add(stop1);
+    stopsIn.add(stop2);
+    List<Double> distancesIn = new ArrayList<>();
+    distancesIn.add(0.843774422231134);
+    List<Double> probabilitiesIn = new ArrayList<Double>();
+    probabilitiesIn.add(.025);
+    probabilitiesIn.add(0.3);
+    PassengerGenerator generatorIn = new RandomPassengerGenerator(stopsIn, probabilitiesIn);
+
+    Route testRouteIn = new Route(0, "testRouteIn",
+            stopsIn, distancesIn, generatorIn);
+
+    List<Stop> stopsOut = new ArrayList<Stop>();
+    stopsOut.add(stop2);
+    stopsOut.add(stop1);
+    List<Double> distancesOut = new ArrayList<>();
+    distancesOut.add(0.843774422231134);
+    List<Double> probabilitiesOut = new ArrayList<Double>();
+    probabilitiesOut.add(0.3);
+    probabilitiesOut.add(.025);
+    PassengerGenerator generatorOut = new RandomPassengerGenerator(stopsOut, probabilitiesOut);
+
+    Route testRouteOut = new Route(1, "testRouteOut",
+            stopsOut, distancesOut, generatorOut);
+
+    Train testTrain = new DieselTrain(1, new Line(10000, "testLine", "BUS",
+            testRouteOut, testRouteIn, new Issue()), 3, 1.0);
+
+    assertEquals(3, trainFactory.getStorageFacility().getElectricTrainsNum());
+    assertEquals(3, trainFactory.getStorageFacility().getDieselTrainsNum());
+    trainFactory.returnVehicle(testTrain);
+    assertEquals(3, trainFactory.getStorageFacility().getElectricTrainsNum());
+    assertEquals(4, trainFactory.getStorageFacility().getDieselTrainsNum());
+
+  }
+>>>>>>> 1be9057 (added tests for TrainFactory)
 }
